889a2ff65f35cf3bbaf6f7ae900e856be7634156
Sebin Suresh <ssuresh@nvisia.com>
2024-01-04 17:55:03 -0600

    Angular: Add service, update component, etc. for listing employees

---
+++ frontend/src/app/employee.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Employee } from './employee';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  // If this was not specified here, need to add
+  // `EmployeeService` to app.module.ts's `providers` array
+  providedIn: 'root',
+})
+export class EmployeeService {
+  private apiServerUrl = environment.apiBaseUrl;
+
+  constructor(private http: HttpClient) {}
+
+  public getEmployees(): Observable<Employee[]> {
+    return this.http.get<Employee[]>(`${this.apiServerUrl}/employee/all`);
+  }
+
+  public addEmployee(employee: Employee): Observable<Employee> {
+    return this.http.post<Employee>(`${this.apiServerUrl}/employee/add`, employee);
+  }
+
+  public updateEmployee(employee: Employee): Observable<Employee> {
+    return this.http.put<Employee>(`${this.apiServerUrl}/employee/update`, employee);
+  }
+
+  public deleteEmployee(employeeId: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiServerUrl}/employee/delete/${employeeId}`);
+  }
+}
