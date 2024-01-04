1f42cadb899f356a95868b585a8866d46f640685
Sebin Suresh <ssuresh@nvisia.com>
2024-01-04 17:54:15 -0600

    Allow CORS origin from localhost:4200

--- backend/src/main/java/dev/sebinsuresh/employeemanager/EmployeemanagerApplication.java
+++ backend/src/main/java/dev/sebinsuresh/employeemanager/EmployeemanagerApplication.java
@@ -2,6 +2,9 @@ package dev.sebinsuresh.employeemanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class EmployeemanagerApplication {
@@ -10,4 +13,13 @@ public class EmployeemanagerApplication {
 		SpringApplication.run(EmployeemanagerApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/employee/**").allowedOrigins("http://localhost:4200");
+			}
+		};
+	}
 }
