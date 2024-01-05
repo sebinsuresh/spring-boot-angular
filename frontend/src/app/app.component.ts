77d0bfb5f377ce177282af143c98e0f3386917fe
Sebin Suresh <ssuresh@nvisia.com>
2024-01-05 14:50:06 -0600

    Add modals and way to create, update, delete Employees

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Employee } from './employee';
 import { EmployeeService } from './employee.service';
 import { HttpErrorResponse } from '@angular/common/http';
+import { ModalModes } from 'src/types/modalModes';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +12,7 @@ import { HttpErrorResponse } from '@angular/common/http';
 })
 export class AppComponent implements OnInit {
   public employees: Employee[] = [];
+  public currentEmployee: Employee | null = null;
 
   constructor(private employeeService: EmployeeService) {}
 
@@ -26,6 +29,94 @@ export class AppComponent implements OnInit {
       error: (error: HttpErrorResponse) => {
         console.error(error.message);
         this.employees = [];
+      },
+    });
+  }
+
+  public logMessage(...args: any[]) {
+    console.log(...args);
+  }
+
+  public onOpenModal(employee: Employee | null, mode: ModalModes) {
+    this.currentEmployee = employee;
+
+    const modalContainer = document.getElementById('modals-container');
+    if (!modalContainer) {
+      console.error('modal container not found');
+      return;
+    }
+    modalContainer.style.display = 'block';
+
+    const modal = document.getElementById(`${mode}Employee`);
+    if (!modal) {
+      console.error(`modal for ${mode} not found`);
+      return;
+    }
+    modal.style.display = 'block';
+  }
+
+  public closeAllModals() {
+    const modals = document.querySelectorAll(`#modals-container .modal`);
+    modals.forEach((modal) => {
+      if (!modal || !(modal instanceof HTMLElement)) return;
+      modal.style.display = 'none';
+    });
+
+    const modalContainer = document.getElementById('modals-container');
+    if (!modalContainer) {
+      console.error('modal container not found');
+      return;
+    }
+    modalContainer.style.display = 'none';
+  }
+
+  public onAddEmployee(form: NgForm) {
+    this.closeAllModals();
+    this.employeeService
+      .addEmployee(form.value)
+      .subscribe({
+        next: () => {
+          this.getEmployees();
+        },
+        error: (error) => {
+          console.error(error);
+        },
+      })
+      .add(() => {
+        form.resetForm();
+      });
+  }
+
+  public onEditEmployee(employee: Employee | null) {
+    if (!employee) {
+      console.error('No employee selected for edit');
+      return;
+    }
+
+    this.closeAllModals();
+    this.employeeService.updateEmployee(employee).subscribe({
+      next: () => {
+        this.getEmployees();
+      },
+      error: (error) => {
+        console.error(error);
+      },
+    });
+  }
+
+  public onDeleteEmployee(employee: Employee | null) {
+    if (!employee) {
+      console.error('No employee selected for edit');
+      return;
+    }
+
+    this.closeAllModals();
+    this.employeeService.deleteEmployee(employee.id).subscribe({
+      next: () => {
+        this.getEmployees();
+      },
+      error: (error) => {
+        console.error(error);
       },
     });
   }
