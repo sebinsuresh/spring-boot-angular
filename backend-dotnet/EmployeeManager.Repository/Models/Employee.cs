f717f48c4730eee996815657c0a445e250c9ba0c
Sebin Suresh <ssuresh@nvisia.com>
2024-01-23 18:28:15 -0600

    Add snake case attributes for table and columns to match existing postgres data

--- backend-dotnet/EmployeeManager.Repository/Models/Employee.cs
+++ backend-dotnet/EmployeeManager.Repository/Models/Employee.cs
@@ -1,13 +1,29 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace EmployeeManager.Repository.Models;
 
+// TODO: Fix warnings
+[Table("employee")]
 public class Employee
 {
+    [Column("id")]
     public long Id { get; set; }
-    // TODO: These warnings
+
+    [Column("name")]
     public string Name { get; set; }
+
+    [Column("email")]
     public string Email { get; set; }
+
+    [Column("job_title")]
     public string JobTitle { get; set; }
+
+    [Column("phone")]
     public string Phone { get; set; }
+
+    [Column("image_url")]
     public string ImageUrl { get; set; }
+
+    [Column("employee_code")]
     public string EmployeeCode { get; set; }
 }
